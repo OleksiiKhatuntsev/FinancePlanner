490bfd92b55eb941b7bf9a148f2dc774df2e270b
Oleksii Khatuntsev <oleksii_khatuntsev@symbotic.com>
2023-05-27 13:05:31 +0200

    add api controller

---
+++ API/Controllers/OperationTypesController.cs
@@ -0,0 +1,21 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("/api/operationTypes")]
+    public class OperationTypesController : BaseApiController
+    {
+        public OperationTypesController(DataContext context) : base(context) { }
+
+        [HttpGet]
+        public async Task<ActionResult<List<OperationType>>> GetOperationTypes() => await Context.OperationTypes.ToListAsync();
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OperationType>> GetOperationType(Guid id) => await Context.OperationTypes.FindAsync(id);
+
+    }
+}
